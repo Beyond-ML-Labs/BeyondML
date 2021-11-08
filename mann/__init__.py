3a21af8c865af6b9338a011efcbc2893e065a6e9
Jacob Renn <jacob.renn@squared.ai>
2021-11-08 09:07:27 -0500

    adding package init

---
+++ mann/__init__.py
@@ -0,0 +1,12 @@
+"""
+MANN - Multitask Artificial Neural Network package
+
+This package contains custom utilities and layers to use to build Multitask Artificial Neural Network (MANN) models
+in conjunction with TensorFlow and Keras.
+"""
+
+__version__ = '0.1.0'
+__dev__ = True
+
+import mann.layers
+import mann.utils

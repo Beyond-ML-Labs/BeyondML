3e4416c92111be05bd89b8e9dd218ce263b0af90
Jacob Renn <jacob.renn@squared.ai>
2021-11-12 07:34:10 -0500

    updating version to include dev

--- mann/__init__.py
+++ mann/__init__.py
@@ -5,7 +5,7 @@ This package contains custom utilities and layers to use to build Multitask Arti
 in conjunction with TensorFlow and Keras.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.0dev'
 __dev__ = True
 
 import mann.layers

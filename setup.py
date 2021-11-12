7e43ae866598e50d00c0830b4ca42d0deb24aeab
Jacob Renn <jacob.renn@squared.ai>
2021-11-12 07:47:31 -0500

    changing setup to use setuptools instead of distutils.core

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 from mann import __version__
 
 setup(
     name = 'mann',
     version = __version__,
+    url = 'https://github.com/AISquaredInc/mann',
     packages = ['mann', 'mann.layers', 'mann.utils'],
     author = 'Jacob Renn',
     author_email = 'jacob.renn@squared.ai',
     description = 'Package containing utilities for implementing RSN2/MANN',
-    long_description = open('README.md').read()
+    long_description = open('README.md').read(),
+    license = 'Apache 2.0',
+    install_requires = [
+        'numpy'
+    ]
 )

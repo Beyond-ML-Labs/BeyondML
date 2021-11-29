675fb6db1d5169b9ed42ed2b5ba6e3b23282fc85
Jacob Renn <jacob.renn@squared.ai>
2021-11-29 06:57:55 -0500

    Adding to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author_email = 'mann@squared.ai',
     description = 'Package containing utilities for implementing RSN2/MANN',
     long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
     license = 'Apache 2.0',
     install_requires = install_requires
 )

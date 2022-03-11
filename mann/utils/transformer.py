261c6d07665dd64149ac4ad5c1a726d188aa7fe6
Jacob Renn <jacob.renn@squared.ai>
2022-03-11 15:02:14 -0500

    updating documentation

--- mann/utils/transformer.py
+++ mann/utils/transformer.py
@@ -8,6 +8,28 @@ def build_transformer_block(
     neurons,
     dropout_rate = 0.1
 ):
+    """
+    Build a Transformer Block
+
+    Parameters
+    ----------
+    input_shape : int or tuple of int
+        The input shape for the model to use
+    embed_dim : int
+        The dimension of the embedding
+    num_heads : int
+        The number of attention heads to use
+    neurons : int
+        The number of hidden neurons to use in the hidden layer
+    dropout_rate : float (default 0.1)
+        Rate at which dropout is applied
+
+    Returns
+    -------
+    transformer_block : TensorFlow keras Functional model
+        The transformer block, which can then be used alone or as 
+        a layer in another model
+    """
     input_layer = tf.keras.layers.Input(input_shape)
     x = tf.keras.layers.MultiHeadAttention(
         num_heads = num_heads,
@@ -28,6 +50,24 @@ def build_token_position_embedding_block(
     vocab_size,
     embed_dim
 ):
+    """
+    Builds a token and position embedding block
+
+    Parameters
+    ----------
+    sequence_length : int
+        The length of each sequence
+    vocab_size : int
+        The size of the vocabulary used
+    embed_dim : int
+        The desired embedding dimension
+
+    Returns
+    -------
+    embedding_block : TensorFlow keras Functional model
+        The embedding block, which can be used alone or 
+        as a layer in another model
+    """
     tok_input = tf.keras.layers.Input(sequence_length)
     pos_input = tf.keras.layers.Input(sequence_length)
 

33ac6365e3a710159ed46f86a1e9f2f89a037bd0
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-06-05 12:30:48 -0400

    adding multi layers

---
+++ mann/burning/layers/MultiDense.py
@@ -0,0 +1,20 @@
+import torch
+
+class MultiDense(torch.nn.Module):
+
+    def __init__(
+            self,
+            weight,
+            bias
+    ):
+        super().__init__()
+        self.w = torch.Tensor(weight).to_sparse()
+        self.b = torch.Tensor(bias)
+
+    def forward(self, inputs):
+        outputs = []
+        for i in range(len(inputs)):
+            out = torch.sparse.mm(inputs[i], self.w[i])
+            out = torch.add(out, self.b[i])
+            outputs.append(out)
+        return outputs
--- mann/burning/layers/__init__.py
+++ mann/burning/layers/__init__.py
@@ -4,3 +4,5 @@ from .MaskedDense import MaskedDense
 from .MaskedConv2D import MaskedConv2D
 from .MultiMaskedDense import MultiMaskedDense
 from .MultiMaskedConv2D import MultiMaskedConv2D
+from .MultiDense import MultiDense
+from .MultiConv2D import MultiConv2D

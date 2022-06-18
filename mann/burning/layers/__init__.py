27f340cb0fddf3d86c1f355647f80225ff0f3816
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-06-18 14:14:34 -0400

    updating file names and imports

--- mann/burning/layers/__init__.py
+++ mann/burning/layers/__init__.py
@@ -4,7 +4,7 @@ from .MaskedDense import MaskedDense
 from .MaskedConv2D import MaskedConv2D
 from .MultiMaskedDense import MultiMaskedDense
 from .MultiMaskedConv2D import MultiMaskedConv2D
-from .MultiDense import MultiDense
-from .MultiConv2D import MultiConv2D
+from .SparseMultiDense import SparseMultiDense
+from .SparseMultiConv2D import SparseMultiConv2D
 from .SparseDense import SparseDense
 from .SparseConv2D import SparseConv2D

b1e5ffa30a2e08339ca2b2d939cee615a025ab88
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-07-28 13:58:13 -0400

    Updating documentation
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- beyondml/tflow/__init__.py
+++ beyondml/tflow/__init__.py
@@ -1,7 +1,7 @@
 """
 TensorFlow compatibility for building MANN models.
 
-The `beyondml.tflow` package contains two subpackages, `layers` and `utils`, which contain
+The `beyondml.tflow` package contains two subpackages, `beyondml.tflow.layers` and `beyondml.tflow.utils`, which contain
 the functionality to create and train MANN layers within TensorFlow. For individuals who are
 familiar with the former name of this package, `mann`, backwards compatibility can be achieved
 (assuming only TensorFlow support is needed), by replacing the following line of code:
@@ -13,6 +13,31 @@ with the following line:
 >>> import beyondml.tflow as mann
 
 in all existing scripts.
+
+Within the `layers` package, there is current functionality for the the following layers:
+- `beyondml.tflow.layers.FilterLayer`
+- `beyondml.tflow.layers.MaskedConv2D`
+- `beyondml.tflow.layers.MaskedDense`
+- `beyondml.tflow.layers.MultiConv2D`
+- `beyondml.tflow.layers.MultiDense`
+- `beyondml.tflow.layers.MultiMaskedConv2D`
+- `beyondml.tflow.layers.MultiMaskedDense`
+- `beyondml.tflow.layers.MultiMaxPool2D`
+- `beyondml.tflow.layers.SelectorLayer`
+- `beyondml.tflow.layers.SumLayer`
+
+Within the `utils` package, there are the current functions and classes:
+- `ActiveSparsification`
+- `build_transformer_block`
+- `build_token_position_embedding_block`
+- `get_custom_objects`
+- `mask_model`
+- `remove_layer_masks`
+- `add_layer_masks`
+- `quantize_model`
+- `get_task_masking_gradients`
+- `mask_task_weights`
+- `train_model_iteratively`
 """
 
 import beyondml.tflow.layers

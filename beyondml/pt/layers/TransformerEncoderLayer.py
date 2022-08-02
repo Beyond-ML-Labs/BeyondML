0b72aa968201f0b82634db7ac18bbeb747f4db0c
Ian Sotnek <ian.sotnek@gmail.com>
2022-08-02 00:12:06 -0400

    Update TransformerEncoderLayer.py

--- beyondml/pt/layers/TransformerEncoderLayer.py
+++ beyondml/pt/layers/TransformerEncoderLayer.py
@@ -82,7 +82,7 @@ class TransformerEncoderLayer(torch.nn.Module):
         x = src
 
         self._sa_block(x)
-        self._ff_block(x)
+        self._ff_block(x).prune(percetile)
 
         return x
         

ff81f2a53d336f99be51d902f876b65d0684401f
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-10-07 15:35:32 -0400

    removing sparsification test for 3d layer
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- tests/test_tf.py
+++ tests/test_tf.py
@@ -332,35 +332,3 @@ def test_sparse():
         [input1, input2],
         [out1, out2, out3]
     )
-
-    model = build_3d_model()
-    model.compile(loss='mse', optimizer='adam')
-
-    input1 = tf.keras.layers.Input((10, 10, 10, 3))
-    input2 = tf.keras.layers.Input((10, 10, 10, 3))
-    x = beyondml.tflow.layers.SparseMultiConv3D.from_layer(
-        model.layers[2])([input1, input2])
-    x = beyondml.tflow.layers.SparseMultiConv3D.from_layer(model.layers[3])(x)
-    x = beyondml.tflow.layers.MultiMaxPool3D()(x)
-    x1 = beyondml.tflow.layers.SelectorLayer(0)(x)
-    x2 = beyondml.tflow.layers.SelectorLayer(1)(x)
-    x1 = beyondml.tflow.layers.SparseConv3D.from_layer(model.layers[7])(x1)
-    x2 = beyondml.tflow.layers.SparseConv3D.from_layer(model.layers[8])(x2)
-    x1 = tf.keras.layers.Flatten()(x1)
-    x2 = tf.keras.layers.Flatten()(x2)
-    x1 = beyondml.tflow.layers.SparseDense.from_layer(model.layers[11])(x1)
-    x2 = beyondml.tflow.layers.SparseDense.from_layer(model.layers[12])(x2)
-    x = beyondml.tflow.layers.SparseMultiDense.from_layer(
-        model.layers[13])([x1, x2])
-    x = beyondml.tflow.layers.SparseMultiDense.from_layer(model.layers[14])(x)
-    x1 = beyondml.tflow.layers.SelectorLayer(0)(x)
-    x2 = beyondml.tflow.layers.SelectorLayer(1)(x)
-    x1 = beyondml.tflow.layers.FilterLayer()(x1)
-    x2 = beyondml.tflow.layers.FilterLayer(False)(x2)
-    out1 = x1
-    out2 = x2
-    out3 = beyondml.tflow.layers.SumLayer()([x1, x2])
-    sparse_model = tf.keras.models.Model(
-        [input1, input2],
-        [out1, out2, out3]
-    )
